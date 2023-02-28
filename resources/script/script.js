569567d936bc65f313831133279cd93d142671b9
TheRealOne78 <bajcsielias78@gmail.com>
2023-03-01 01:02:45 +0200

    Add licensing and cleaner code
    Fix image
    
    Fix default fun fact

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,4 +1,7 @@
-const animePathArr = [
+/* Images paths *
+ * In this case, the paths are relative to 'homepage.html',
+ * but absolute paths can also be set with no problem */
+const imgPathArr = [
   "./resources/img/anime/1.png",
   "./resources/img/anime/2.png",
   "./resources/img/anime/3.jpg",
@@ -10,14 +13,20 @@ const animePathArr = [
   "./resources/img/anime/9.png"
 ]
 
-const randomImage =animePathArr[Math.floor(Math.random() * animePathArr.length)]
-document.getElementById("img").src =randomImage
+/* Get a random path from imgPathArr and set it in HTML */
+const randImgPath = imgPathArr[Math.floor(Math.random() * imgPathArr.length)]
+document.getElementById("img").src = randImgPath
 
+/* Pre-defined terminal-like HEX colors */
 const textColorArr = [ "#ffffff", "#0037DA", "#3A96DD", "#13A10E", "#881798", "#C50F1F", "#C19C00" ]
 
+/* Get a random HEX color from textColorArr */
 const textColor = Math.floor(Math.random() * textColorArr.length)
-document.body.style.color =textColorArr[textColor]
 
+/* Set the body text color as textColor */
+document.body.style.color = textColorArr[textColor]
+
+/* Set other text color as textColor, just to be safe */
 document.querySelector(".errItem:nth-child(2)").style.color = textColorArr[textColor]
 document.querySelector(".errItemText").style.color = textColorArr[textColor];
 document.querySelectorAll("a").forEach(a => a.style.color = textColorArr[textColor])
