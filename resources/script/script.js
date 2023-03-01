04626612002159ea147f1ace1d58e2e052d1fcd0
TheRealOne78 <bajcsielias78@gmail.com>
2023-03-01 13:44:24 +0200

    Add more pictures

--- resources/script/script.js
+++ resources/script/script.js
@@ -10,7 +10,30 @@ const imgPathArr = [
   "./resources/img/anime/6.jpg",
   "./resources/img/anime/7.png",
   "./resources/img/anime/8.jpg",
-  "./resources/img/anime/9.png"
+  "./resources/img/anime/9.png",
+  "./resources/img/anime/wallhaven-1k136g.jpg",
+  "./resources/img/anime/wallhaven-28wrdx.jpg",
+  "./resources/img/anime/wallhaven-2eqkzx.png",
+  "./resources/img/anime/wallhaven-57pd53.jpg",
+  "./resources/img/anime/wallhaven-6oo2yl.jpg",
+  "./resources/img/anime/wallhaven-833gr1.jpg",
+  "./resources/img/anime/wallhaven-966yr8.jpg",
+  "./resources/img/anime/wallhaven-96vg61.jpg",
+  "./resources/img/anime/wallhaven-e7g32o.png",
+  "./resources/img/anime/wallhaven-eyeeo8.jpg",
+  "./resources/img/anime/wallhaven-eyy16o.jpg",
+  "./resources/img/anime/wallhaven-g7pok3.png",
+  "./resources/img/anime/wallhaven-g8ez97.jpg",
+  "./resources/img/anime/wallhaven-m9evmk.png",
+  "./resources/img/anime/wallhaven-md2qzk.jpg",
+  "./resources/img/anime/wallhaven-o33lj5.jpg",
+  "./resources/img/anime/wallhaven-p8oy5m.jpg",
+  "./resources/img/anime/wallhaven-pk6233.jpg",
+  "./resources/img/anime/wallhaven-pkkxg9.jpg",
+  "./resources/img/anime/wallhaven-q66oqq.png",
+  "./resources/img/anime/wallhaven-x88m3l.png",
+  "./resources/img/anime/wallhaven-ymom6x.jpg",
+  "./resources/img/anime/wallhaven-z8ylgj.jpg"
 ]
 
 /* Get a random path from imgPathArr and set it in HTML */
