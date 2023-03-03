83e40afa40d7744dbf7577032ab9407a18689c02
TheRealOne78 <bajcsielias78@gmail.com>
2023-03-03 12:31:02 +0200

    Updated README.md copyright, scaled down images for faster image loading

--- resources/script/script.js
+++ resources/script/script.js
@@ -2,38 +2,38 @@
  * In this case, the paths are relative to 'homepage.html',
  * but absolute paths can also be set with no problem */
 const imgPathArr = [
-  "./resources/img/anime/1.png",
-  "./resources/img/anime/2.png",
-  "./resources/img/anime/3.jpg",
-  "./resources/img/anime/4.png",
-  "./resources/img/anime/5.jpg",
-  "./resources/img/anime/6.jpg",
-  "./resources/img/anime/7.png",
-  "./resources/img/anime/8.jpg",
-  "./resources/img/anime/9.png",
-  "./resources/img/anime/wallhaven-1k136g.jpg",
-  "./resources/img/anime/wallhaven-28wrdx.jpg",
-  "./resources/img/anime/wallhaven-2eqkzx.png",
-  "./resources/img/anime/wallhaven-57pd53.jpg",
-  "./resources/img/anime/wallhaven-6oo2yl.jpg",
-  "./resources/img/anime/wallhaven-833gr1.jpg",
-  "./resources/img/anime/wallhaven-966yr8.jpg",
-  "./resources/img/anime/wallhaven-96vg61.jpg",
-  "./resources/img/anime/wallhaven-e7g32o.png",
-  "./resources/img/anime/wallhaven-eyeeo8.jpg",
-  "./resources/img/anime/wallhaven-eyy16o.jpg",
-  "./resources/img/anime/wallhaven-g7pok3.png",
-  "./resources/img/anime/wallhaven-g8ez97.jpg",
-  "./resources/img/anime/wallhaven-m9evmk.png",
-  "./resources/img/anime/wallhaven-md2qzk.jpg",
-  "./resources/img/anime/wallhaven-o33lj5.jpg",
-  "./resources/img/anime/wallhaven-p8oy5m.jpg",
-  "./resources/img/anime/wallhaven-pk6233.jpg",
-  "./resources/img/anime/wallhaven-pkkxg9.jpg",
-  "./resources/img/anime/wallhaven-q66oqq.png",
-  "./resources/img/anime/wallhaven-x88m3l.png",
-  "./resources/img/anime/wallhaven-ymom6x.jpg",
-  "./resources/img/anime/wallhaven-z8ylgj.jpg"
+  "./resources/img/itemImg/96BYTHT.jpg",
+  "./resources/img/itemImg/anewwall.png",
+  "./resources/img/itemImg/morpho.jpg",
+  "./resources/img/itemImg/wadim-kashin-monday.jpg",
+  "./resources/img/itemImg/wallhaven-137qmv.png",
+  "./resources/img/itemImg/wallhaven-1k136g.jpg",
+  "./resources/img/itemImg/wallhaven-28wrdx.jpg",
+  "./resources/img/itemImg/wallhaven-2eqkzx.png",
+  "./resources/img/itemImg/wallhaven-57pd53.jpg",
+  "./resources/img/itemImg/wallhaven-6oo2yl.jpg",
+  "./resources/img/itemImg/wallhaven-728k2e.png",
+  "./resources/img/itemImg/wallhaven-762zq3.png",
+  "./resources/img/itemImg/wallhaven-833gr1.jpg",
+  "./resources/img/itemImg/wallhaven-966yr8.jpg",
+  "./resources/img/itemImg/wallhaven-96vg61.jpg",
+  "./resources/img/itemImg/wallhaven-e7g32o.png",
+  "./resources/img/itemImg/wallhaven-eyeeo8.jpg",
+  "./resources/img/itemImg/wallhaven-eyy16o.jpg",
+  "./resources/img/itemImg/wallhaven-g7pok3.png",
+  "./resources/img/itemImg/wallhaven-g8ez97.jpg",
+  "./resources/img/itemImg/wallhaven-j55m7p.jpg",
+  "./resources/img/itemImg/wallhaven-m9evmk.png",
+  "./resources/img/itemImg/wallhaven-md2qzk.jpg",
+  "./resources/img/itemImg/wallhaven-o33lj5.jpg",
+  "./resources/img/itemImg/wallhaven-p8oy5m.jpg",
+  "./resources/img/itemImg/wallhaven-pk6233.jpg",
+  "./resources/img/itemImg/wallhaven-pkkxg9.jpg",
+  "./resources/img/itemImg/wallhaven-q66oqq.png",
+  "./resources/img/itemImg/wallhaven-q68d17.png",
+  "./resources/img/itemImg/wallhaven-x88m3l.png",
+  "./resources/img/itemImg/wallhaven-ymom6x.jpg",
+  "./resources/img/itemImg/wallhaven-z8ylgj.jpg"
 ]
 
 /* Get a random path from imgPathArr and set it in HTML */
@@ -50,7 +50,7 @@ const textColor = Math.floor(Math.random() * textColorArr.length)
 document.body.style.color = textColorArr[textColor]
 
 /* Set other text color as textColor, just to be safe */
-document.querySelector(".errItem:nth-child(2)").style.color = textColorArr[textColor]
-document.querySelector(".errItemText").style.color = textColorArr[textColor];
+document.querySelector(".item:nth-child(2)").style.color = textColorArr[textColor]
+document.querySelector(".itemText").style.color = textColorArr[textColor];
 document.querySelectorAll("a").forEach(a => a.style.color = textColorArr[textColor])
 document.querySelectorAll(".selected").forEach(selected => selected.style.background = textColorArr[textColor])
